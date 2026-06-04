cfb36b80e3c06ceb09309458d44dc91cd542be05
Osiel <jarot.fierro@gmail.com>
2026-06-03 23:56:14 -0400

    Agregar campo de filtro por ID y ajustar estilos en tablas de resultados

--- administracion/filtros/comunas.py
+++ administracion/filtros/comunas.py
@@ -2,6 +2,15 @@ from django import forms
 
 
 class ComunaFilterForm(forms.Form):
+    id = forms.IntegerField(
+        required=False,
+        widget=forms.NumberInput(
+            attrs={
+                'class': 'form-control form-control-sm',
+                'placeholder': 'ID...'
+            }
+        )
+    )
     nombre = forms.CharField(
         required=False,
         widget=forms.TextInput(
